52f9ec2bbde7795cce44a4ee9cd45153f267a33d
Tiagofilos <159203903+Tiagofilos@users.noreply.github.com>
2026-04-25 22:02:08 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ export default function App() {
   return (
     <div style={{
       minHeight: "100vh",
-      background: "red",
+      background: "green",
       color: "white",
       fontSize: "40px",
       textAlign: "center",
       paddingTop: "100px"
     }}>
-      TESTE FUNCIONANDO
+      REACT FUNCIONANDO
     </div>
   );
 }
